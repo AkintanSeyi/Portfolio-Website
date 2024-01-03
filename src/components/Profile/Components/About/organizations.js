cc0aee2793d4d0b73fa8a9d9bdc9b843fb8cbd92
Akingz2000 <seyiakintan@yahoo.com>
2024-01-03 05:59:06 -0800

    portfolio webapp

---
+++ src/components/Profile/Components/About/organizations.js
@@ -0,0 +1,80 @@
+ var  organization = [
+
+
+   {
+
+      companyName : "NextGenTech",
+      Position : "Software Developer",
+      companyLocation : "England",
+      type : "Remote",
+      dateStarted : "Dec 2022",
+      dateEnded : "till Date",
+      aboutOne : "Implemented enhancements that improved web functionality and responsiveness ",
+      aboutTwo : "Designed and maintained both corporate and client websites using script languages",
+       aboutThree : "Enable site wide promotions by programming HTML5 canvases to animate particles on web backgrounds",
+       aboutFour : "Built cross browser compatible and accessibility complaint website , resulting in 22% faster load time ."
+
+   },
+
+
+   {
+      companyName : "SynOption",
+      Position : "Software Developer",
+      companyLocation : "Singapore",
+      type : "Remote",
+      dateStarted : "May 2022",
+      dateEnded : "Jan 2023",
+      aboutOne : "Collaborated with different software developer from all over the world creating programs to work for the company purpose.",
+      aboutTwo : " Created programs with React js and JavaScript and connected it to the backend.",
+       aboutThree : "Made complex designs which was later implemented into the company designs. ",
+
+
+
+   },
+
+   {
+      companyName : "Fiverr",
+      Position : " Frontend Developer",
+      companyLocation : "Lagos, Nigeria",
+      type : "Contract",
+      dateStarted : "Sep 2021",
+      dateEnded : "Apr 2022",
+      aboutOne : "Made and deployed UI pages using reactjs",
+      aboutTwo : "Worked on over 20+ project rendering flexible and responsive design for the customers ",
+       aboutThree : "Fixed Code Bug for clients to increase the code functionality and reduce slowness of the app",
+       aboutFour : "Made Full stack website using Reactjs and Node as the Backend , published it online for the client also"
+
+
+   },
+
+   {
+      companyName : "Sofa",
+      Position : "Web Designer",
+      companyLocation : "Nigeria",
+      type : "Full Time",
+      dateStarted : "jan 2021",
+      dateEnded : "Nov 2021",
+      aboutOne : "Made and Design the company Website using wordpress and Html and css",
+      aboutTwo : "Made an chatting Platform to chat with customers on the website",
+       aboutThree : "Designed the clients website from beginning to end , styling , animation etc",
+       aboutFour : "Made an Eccomerce functionality on wix and wordpress for the company clients",
+
+
+
+   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ]
+
+ export default organization
---
+++ src/components/Profile/Components/Skills/skillsData.js
@@ -0,0 +1,69 @@
+
+
+var skillsData = {
+
+
+    FrontEnd : [
+        "Javascript",
+        "Typescript",
+        "React JS",
+        "Next JS",
+        "Redux",
+        "React Native",
+        "Styled Components",
+        "SCSS",
+     "Material UI",
+    "Tailwind CSS",
+       "Bootstrap",
+           "D3",
+           "RxJS",
+          "Chart JS",
+          "Plotly JS",
+    ],
+
+    Backend : [
+        "Node js",
+        "Express Js",
+         "Mongo DB",
+         "GraphQL",
+         "Web Socket"
+
+    ],
+
+    other : [
+        "Github",
+           "Gitlab",
+        "Heroku",
+        "AWS",
+        "Vercel",
+        "Willing to learn new technology",
+
+    ],
+
+
+    SoftSkills : [
+        "Excellent Communication Skill",
+       "Attention to details",
+       "Problem solving skills",
+       "Working continuously on a task until completion",
+       "Ability to work well in a team",
+       "Logical thinking",
+       "Ability to work well with a team",
+
+    ],
+
+
+    Web_Design : [
+        "wix",
+        "wordpress",
+        "squarespace",
+        "go daddy",
+        "elementor",
+
+    ]
+
+
+}
+
+
+export   default  skillsData;
