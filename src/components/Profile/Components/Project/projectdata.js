97cc6fced263829b4a21507bd54368a343fd14e9
Akingz2000 <seyiakintan@yahoo.com>
2024-01-13 16:39:08 -0800

    portfolio webapp

--- src/components/Profile/Components/Project/projectdata.js
+++ src/components/Profile/Components/Project/projectdata.js
@@ -8,6 +8,7 @@ import food from "../../../../images/food.png";
 import funiture from "../../../../images/funiture.png";
 import kwikapp from "../../../../images/kwikappimg.png";
 import jobhubimg from "../../../../images/jobhubimg.png";
+import risevest from "../../../../images/risevest.png";
 
 const projectdata = [
   {
@@ -20,13 +21,22 @@ const projectdata = [
     switchSide: true,
   },
   {
+    id: 12,
+    image: risevest,
+    name: "risvest App",
+    git: "https://github.com/AkintanSeyi/risevest",
+    web: "https://patriciason-kwikapp.vercel.app/",
+    p: "risvest App is used for saving money indollar and different currency, one of the biggest saving app in africa , This is the clone app ",
+    switchSide: false,
+  },
+  {
     id: 11,
     image: jobhubimg,
     name: "jobhub-Webapp",
     git: "",
     web: "https://jobhubwebapp.vercel.app/",
     p: "Full-stack web app for hunting for jobs and seeking for employee for your company , this uses an free heroku hosting platform , which takes 30 seconds to kick start afte being idle for 30min, Thanks for your patience ",
-    switchSide: false,
+    switchSide: true,
   },
   {
     id: 2,
@@ -35,7 +45,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/funiture",
     web: "https://akintanseyi.github.io/funiture/",
     p: "this is a website of slick-funitures, they sell different kind of funitures of all kind, they also offer delivey of product free and offer a great product at afforable rate",
-    switchSide: true,
+    switchSide: false,
   },
   {
     id: 9,
@@ -44,7 +54,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/fibre-",
     web: "https://akintanseyi.github.io/fibre-/",
     p: "fibre is the number one leading housing company in lagos, having over 100 buildings and hundreds of agent nationwide",
-    switchSide: false,
+    switchSide: true,
   },
   {
     id: 1,
@@ -53,7 +63,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/car",
     web: "https://akintanseyi.github.io/car/",
     p: "this is a website about as-cars company, top leading car dealers company in west-africa, they sold over 5000 cars and are know as revolutionarizing selling of cars",
-    switchSide: true,
+    switchSide: false,
   },
 
   {
@@ -63,7 +73,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/mother-earth",
     web: "https://akintanseyi.github.io/mother-earth/",
     p: "this is a webite about our beautiful planet, different amazing pictures from all over the world",
-    switchSide: false,
+    switchSide: true,
   },
   {
     id: 5,
@@ -72,7 +82,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/housing-website",
     web: "https://akintanseyi.github.io/housing-website/",
     p: "housing in lagos is an new compny with just took off 2 years ago, they are making waves and are know for providing houses in lagos state",
-    switchSide: true,
+    switchSide: false,
   },
   {
     id: 6,
@@ -81,7 +91,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/nft",
     web: "https://akintanseyi.github.io/nft/",
     p: "this is a website about nft pictures and beautiful pictures from different nft products",
-    switchSide: false,
+    switchSide: true,
   },
 
   {
@@ -91,7 +101,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/freshmenu",
     web: "https://akintanseyi.github.io/freshmenu/",
     p: "this is a new india resturant which is making waves right now, have over 20 stores nationwide and make amazing meals",
-    switchSide: true,
+    switchSide: false,
   },
   {
     id: 4,
@@ -100,7 +110,7 @@ const projectdata = [
     git: "https://github.com/akintanseyi/cream",
     web: "https://akintanseyi.github.io/cream/",
     p: "this is a website about the leading cosmetic comapny in ipaye region lagos state, sold over 100,000 product and nominated for the CSA award 2021",
-    switchSide: false,
+    switchSide: true,
   },
 ];
 
