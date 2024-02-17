c529042d485c6772200ea88754e82358bba13bd0
unknown <seyiakintan@yahoo.com>
2024-02-17 06:57:37 -0800

    Portfolio

---
+++ src/components/Profile/Components/Project/projectdata.js
@@ -0,0 +1,117 @@
+import cream from "../../../../images/cream.png";
+import fibre from "../../../../images/fibre.png";
+import cars from "../../../../images/cars.png";
+import walpaper from "../../../../images/walpaper.png";
+import housing from "../../../../images/housing.png";
+import nft from "../../../../images/nft.png";
+import food from "../../../../images/food.png";
+import funiture from "../../../../images/funiture.png";
+import kwikapp from "../../../../images/kwikappimg.png";
+import jobhubimg from "../../../../images/jobhubimg.png";
+import risevest from "../../../../images/risevest.png";
+
+const projectdata = [
+  {
+    id: 10,
+    image: kwikapp,
+    name: "kwik.delivery Clone",
+    git: "https://github.com/AkintanSeyi/kwikapp",
+    web: "https://patriciason-kwikapp.vercel.app/",
+    p: "kwik.delivery is one of the biggest delivery app in africa and this is an clone of the web app",
+    switchSide: true,
+  },
+  {
+    id: 12,
+    image: risevest,
+    name: "risvest App",
+    git: "https://github.com/AkintanSeyi/risevest",
+    web: "https://patriciason-risevest.vercel.app/",
+    p: "risvest App is used for saving money indollar and different currency, one of the biggest saving app in africa , This is the clone app ",
+    switchSide: false,
+  },
+  {
+    id: 11,
+    image: jobhubimg,
+    name: "jobhub-Webapp",
+    git: "",
+    web: "https://jobhubwebapp.vercel.app/",
+    p: "Full-stack web app for hunting for jobs and seeking for employee for your company , this uses an free heroku hosting platform , which takes 30 seconds to kick start afte being idle for 30min, Thanks for your patience ",
+    switchSide: true,
+  },
+  {
+    id: 2,
+    image: funiture,
+    name: "slick-funiture",
+    git: "https://github.com/akintanseyi/funiture",
+    web: "https://akintanseyi.github.io/funiture/",
+    p: "this is a website of slick-funitures, they sell different kind of funitures of all kind, they also offer delivey of product free and offer a great product at afforable rate",
+    switchSide: false,
+  },
+  {
+    id: 9,
+    image: fibre,
+    name: "fibre",
+    git: "https://github.com/akintanseyi/fibre-",
+    web: "https://akintanseyi.github.io/fibre-/",
+    p: "fibre is the number one leading housing company in lagos, having over 100 buildings and hundreds of agent nationwide",
+    switchSide: true,
+  },
+  {
+    id: 1,
+    image: cars,
+    name: "as-cars",
+    git: "https://github.com/akintanseyi/car",
+    web: "https://akintanseyi.github.io/car/",
+    p: "this is a website about as-cars company, top leading car dealers company in west-africa, they sold over 5000 cars and are know as revolutionarizing selling of cars",
+    switchSide: false,
+  },
+
+  {
+    id: 3,
+    image: walpaper,
+    name: "mother-earth",
+    git: "https://github.com/akintanseyi/mother-earth",
+    web: "https://akintanseyi.github.io/mother-earth/",
+    p: "this is a webite about our beautiful planet, different amazing pictures from all over the world",
+    switchSide: true,
+  },
+  {
+    id: 5,
+    image: housing,
+    name: "housing in lagos",
+    git: "https://github.com/akintanseyi/housing-website",
+    web: "https://akintanseyi.github.io/housing-website/",
+    p: "housing in lagos is an new compny with just took off 2 years ago, they are making waves and are know for providing houses in lagos state",
+    switchSide: false,
+  },
+  {
+    id: 6,
+    image: nft,
+    name: "nft-store",
+    git: "https://github.com/akintanseyi/nft",
+    web: "https://akintanseyi.github.io/nft/",
+    p: "this is a website about nft pictures and beautiful pictures from different nft products",
+    switchSide: true,
+  },
+
+  {
+    id: 7,
+    image: food,
+    name: "freash-menu",
+    git: "https://github.com/akintanseyi/freshmenu",
+    web: "https://akintanseyi.github.io/freshmenu/",
+    p: "this is a new india resturant which is making waves right now, have over 20 stores nationwide and make amazing meals",
+    switchSide: false,
+  },
+  {
+    id: 4,
+    image: cream,
+    name: "l&d lotions",
+    git: "https://github.com/akintanseyi/cream",
+    web: "https://akintanseyi.github.io/cream/",
+    p: "this is a website about the leading cosmetic comapny in ipaye region lagos state, sold over 100,000 product and nominated for the CSA award 2021",
+    switchSide: true,
+  },
+];
+
+export default projectdata;
